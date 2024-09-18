d9cbc7f6c2ea4c60a0ffb2edde53008bb6791546
Jesse van Gool <jvgool2001@gmail.com>
2024-09-18 15:17:20 +0200

    Mutex

--- module_visitor.go
+++ module_visitor.go
@@ -1,25 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 type ModuleVisitor interface {
 	VisitCompressor(*Compressor)
 	VisitDispenser(*Dispenser)
 }
 
-type ReportGeneratorVisitor struct{}
+type ReportGeneratorVisitor struct {
+	mu sync.Mutex
+}
 
 func (v *ReportGeneratorVisitor) VisitCompressor(c *Compressor) {
+	v.mu.Lock()
 	fmt.Println("Generating report for Compressor")
 	data := c.ControlData.Data["compressor"].(CompressorData)
 	data.ValueY = 20
 	c.ControlData.Data["compressor"] = data
+	v.mu.Unlock()
 
 }
 
 func (v *ReportGeneratorVisitor) VisitDispenser(d *Dispenser) {
+	v.mu.Lock()
 	fmt.Println("Generating report for dispenser")
 	data := d.ControlData.Data["dispenser"].(DispenserData)
 	data.ValueB = 241.123
 	d.ControlData.Data["dispenser"] = data
+	v.mu.Unlock()
 }
